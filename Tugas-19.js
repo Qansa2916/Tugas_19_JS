adace31b22f61c175cf8a618f1a1c04678882d46
Qansa Alghifari <46017824+Qansa2916@users.noreply.github.com>
2019-11-04 10:42:46 +0700

    Tugas_19_JS

---
+++ Tugas-19.js
@@ -0,0 +1,24 @@
+function x() {
+    var y = "Semangat...";
+    var z = "****";
+    console.log(y.repeat(5));
+    console.log(z);
+    console.log(z.repeat(5));
+    console.log(z.repeat(10));
+    console.log(z.repeat(15));
+    console.log(z.repeat(20));
+    console.log(z.repeat(15));
+    console.log(z.repeat(10));
+    console.log(z.repeat(5));
+    console.log(z.repeat(1));
+
+
+
+
+
+
+
+
+
+}
+x();
